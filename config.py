3f064c6f5e4d6e87b3e3e58ce3b4d8312e2905b6
eddy <eddyyonnie@gmail.com>
2019-04-29 00:19:35 +0300

    config.py rearranged where necessary

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+class Config:
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+
+    MAIL_SERVER ='smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    SUBJECT_PREFIX = 'One Minute Pitch'
+    SENDER_EMAIL = 'eddyyonnie@gmail.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass   
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitch'
+    DEBUG = True
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:12345@localhost/pitch'
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
